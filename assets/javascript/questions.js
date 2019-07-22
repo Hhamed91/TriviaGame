8b34150e2f48fcd15ead98a78b8cc00f662c4101
user.name <hazem.hamed.h@hotmail.com>
2019-07-21 21:22:26 -0700

    Add 10 questions to the list

---
+++ assets/javascript/questions.js
@@ -0,0 +1,56 @@
+var quizQuestions = [
+    {
+        question: "1-What is Liz’s favorite night-time food?",
+        choices: ["Night chips", "Night fries","Night cheese","Night pizza"],
+        correctAnswer: "Night fries"
+    },
+
+    {
+        question: "2-What is the name of Jack's ex wife?",
+        choices: ["Bianca ", "Sally","Liza","Molly"],
+        correctAnswer: "Bianca "
+    },
+
+    {
+        question: "3-When a clip is shown of Tracy Jordan (Tracy Morgan) running through traffic in his underwear, what is he shouting?",
+        choices: ["I am a Jedi!", "I am Superman!","I am LL!!","I am a lion"],
+        correctAnswer: "I am a Jedi!"
+    },
+    {
+        question: "4-Before Tracy Jordan came onboard the show, who was the star of TGS?",
+        choices: ["Jenna Maroney", "Mindy Lahiri","Leslie Knope","Lucille Bluth"],
+        correctAnswer: "Jenna Maroney"
+    },
+    {
+        question: "5-Liz Lemon works as the head writer for /TGS with Tracy Jordan/. What does TGS stand for?",
+        choices: ["The Girlie Show", " The Gangster Show"," The Gremlin Show","The Grown-Up Show"],
+        correctAnswer: "The Girlie Show"
+    },
+    {
+        question: "6-Kenneth the Page, played by Jack McBrayer, is from which state?",
+        choices: ["Georgia", "Florida","Alabama","New York"],
+        correctAnswer: "Georgia"
+    },
+    {
+        question: "7-On what night does /TGS with Tracy Jordan/ air in the /30 Rock/ universe?",
+        choices: ["Friday", "Monday","Sunday","Tuesday"],
+        correctAnswer: "Friday"
+    },
+    {
+        question: "8-Who is the producer of /TGS/?",
+        choices: ["Pete Hornberger", "Tracy Jorgan","John Donaghey","Liz Lemon"],
+        correctAnswer: "Pete Hornberger"
+    },
+    {
+        question: "9-Which writer does Frank, played by Judah Friedlander, share an office with?",
+        choices: ["Toofer" , "Josh","John","Kenneth"],
+        correctAnswer: "Toofer"
+    },
+    {
+        question: "10-Liz goes on a rampage when she overhears who call her /the C word/",
+        choices: ["Lutz", "Dot Com","Cerie","Grizz"],
+        correctAnswer: "Lutz"
+    }
+
+];
+
