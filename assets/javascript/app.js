f24a300a4331e36b8ade5fe5cb28707ea3e236c4
user.name <hazem.hamed.h@hotmail.com>
2019-07-21 21:39:11 -0700

    Pushing full test one

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,22 +7,22 @@ var lost = 0;
 var timer;
 
 
-// // Start a 30 seconds timer for user to respond or choose an answer to each question
-// function nextQuestion(){
+// Start a 30 seconds timer for user to respond or choose an answer to each question
+function nextQuestion(){
 
-//     var isQuestionOver = (quizQuestions.length - 1 ) === currentQuestion;
+    var isQuestionOver = (quizQuestions.length - 1 ) === currentQuestion;
 
-//     if(isQuestionOver){
+    if(isQuestionOver){
 
-//         // console.log("Game Over")
-//         displayResult();
+        // console.log("Game Over")
+        displayResult();
 
-//     }else{
-//         currentQuestion++;
-//         loadQuestion();
-// }
+    }else{
+        currentQuestion++;
+        loadQuestion();
+}
 
-// };
+};
 
 
 //When 30 seconds are over, show the loss image and clear the timer to reset to 30 sec
@@ -109,73 +109,73 @@ function loadQuestion(){
         // console.log("meh" , selectedAnswer);
     });
 
-    // // For the last page to show the status of the total game
-    // function displayResult(){
-    //     var absLost = Math.abs(lost);
-    //     var result = `
-    //     <p> You got ${score} question(s) right.</p>
-    //     <p> You missed ${absLost} question(s). </p>
-    //     <button class="btn btn-primary" id="reset"> Play Again </button>
-    //     `;
+    // For the last page to show the status of the total game
+    function displayResult(){
+        var absLost = Math.abs(lost);
+        var result = `
+        <p> You got ${score} question(s) right.</p>
+        <p> You missed ${absLost} question(s). </p>
+        <button class="btn btn-primary" id="reset"> Play Again </button>
+        `;
 
-    //     $("#game").html(result)
+        $("#game").html(result)
 
-    // };
+    };
 
-    // //To reset every part of the game once user clicks on rest btn
+    //To reset every part of the game once user clicks on rest btn
 
-    // $(document).on("click", "#reset", function(){
-    //      counter = 30;
-    //      currentQuestion = 0;
-    //      score = 0;
-    //      lost = 0;
-    //      timer = null;
+    $(document).on("click", "#reset", function(){
+         counter = 30;
+         currentQuestion = 0;
+         score = 0;
+         lost = 0;
+         timer = null;
 
-    //      loadQuestion();
+         loadQuestion();
 
-    // });
+    });
 
-    // // It shows the progress of the game like 2/10 questions where answered
-    // function loadRemainingQuestion(){
-    //     var remainingQuesion = quizQuestions.length -(currentQuestion + 1);
-    //     var totalQuestion = quizQuestions.length;
+    // It shows the progress of the game like 2/10 questions where answered
+    function loadRemainingQuestion(){
+        var remainingQuesion = quizQuestions.length -(currentQuestion + 1);
+        var totalQuestion = quizQuestions.length;
 
-    //     return `Remaining Questions: ${remainingQuesion}/${totalQuestion}`;
-    // };
-
-
-    // //Show a giph when correct/false choice
-
-    // function randomImage(images){
-    //     var random = Math.floor(Math.random() * lossImages.length);
-    //     var randomImage = images[random];
-    //     return randomImage;
+        return `Remaining Questions: ${remainingQuesion}/${totalQuestion}`;
+    };
 
 
-    // };
+    //Show a giph when correct/false choice
 
-    // // To pre load an image based on the answer
-    // function prloadImage(status){
-    //     var correctAnswer = quizQuestions[currentQuestion].correctAnswer;
+    function randomImage(images){
+        var random = Math.floor(Math.random() * lossImages.length);
+        var randomImage = images[random];
+        return randomImage;
 
-    //     if(status === "win"){
-    //         $("#game").html(`
-    //         <p class="preload-image"> Congrats, you picked the correct answer </p>
-    //         <p class="preload-image"> Woop Woop!! You got it right. <br> The correct answer is <b> ${correctAnswer} </b> </p>
-    //         <img src="${randomImage(winImages)}"/>
 
-    //             `);
+    };
 
-    //     }else{
-    //         $("#game").html(`
-    //         <p class="preload-image"> The correct answer was ${correctAnswer}</p>
-    //         <p class="preload-image"> You are not a true fan!!</p>
-    //         <img src="${randomImage(lossImages)}"/>
+    // To pre load an image based on the answer
+    function prloadImage(status){
+        var correctAnswer = quizQuestions[currentQuestion].correctAnswer;
+
+        if(status === "win"){
+            $("#game").html(`
+            <p class="preload-image"> Congrats, you picked the correct answer </p>
+            <p class="preload-image"> Woop Woop!! You got it right. <br> The correct answer is <b> ${correctAnswer} </b> </p>
+            <img src="${randomImage(winImages)}"/>
+
+                `);
+
+        }else{
+            $("#game").html(`
+            <p class="preload-image"> The correct answer was ${correctAnswer}</p>
+            <p class="preload-image"> You are not a true fan!!</p>
+            <img src="${randomImage(lossImages)}"/>
                 
-    //             `);
+                `);
 
-    //     }
-    // }
+        }
+    }
 
     
 $("#start").click(function(){
